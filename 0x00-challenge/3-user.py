a2787c6621c9847d2fc6c20763aa9d01c3ba80b3
perrie_d <111010793+perried@users.noreply.github.com>
2023-06-20 07:09:09 +0200

    Update 3-user.py

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -88,8 +88,7 @@ if __name__ == '__main__':
         print("is_valid_password should return True if it's the right \ password") 
   
     if user_1.is_valid_password("Fakepwd"): 
-        print("is_valid_password should return False if it's not the right \ 
-password") 
+        print("is_valid_password should return False if it's not the right \ password") 
   
     if user_1.is_valid_password(None): 
         print("is_valid_password should return False if compare with None") 
@@ -98,5 +97,4 @@ password")
         print("is_valid_password should return False if compare with integer") 
   
     if user_2.is_valid_password("No pwd"): 
-        print("is_valid_password should return False if no password set \ 
-before")
+        print("is_valid_password should return False if no password set \ before")
