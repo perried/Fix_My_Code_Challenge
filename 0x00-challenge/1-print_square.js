fbc2095685077c960bf8bbdf6afe7f0acd3e358b
perrie_d <111010793+perried@users.noreply.github.com>
2023-06-20 06:07:41 +0200

    Update 1-print_square.js

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -14,7 +14,7 @@
      process.exit(1) 
  } 
   
- size = parseInt(process.argv[2], 16) 
+ size = parseInt(process.argv[2], 10) 
   
  for (let i = 0 ; i < size ; i ++) { 
      for (let j = 0 ; j < size ; j ++) { 
